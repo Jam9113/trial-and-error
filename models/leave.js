3d8be39c4297285391c0b565828e85dae1c2b0a1
JAMIE KRISTINA ZOILON <20200019675@my.xu.edu.ph>
2025-06-13 06:26:52 +0800

    Update leave.js

--- models/leave.js
+++ models/leave.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const leaveSchema = new mongoose.Schema({
   employeeName: String,
+  employeeId: String,
   department: String,
-  leaveType: String,
-  startDate: String,
-  endDate: String,
+  leaveType: String,      
+  startDate: Date,
+  endDate: Date,
   reason: String,
-});
+  status: { type: String, default: 'Pending' } 
+}, { timestamps: true });
 
 module.exports = mongoose.model('Leave', leaveSchema);
